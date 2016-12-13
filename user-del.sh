ca39e19dbb51d2eaef8c13c196ab6d551cc3d40d
zihadp <siigoblin78@gmail.com>
2016-12-14 00:43:45 +0700

    Update user-del.sh

--- user-del.sh
+++ user-del.sh
@@ -14,8 +14,11 @@ do
         printf "%-17s %2s\n" "$AKUN" "$exp"
         fi
 done < /etc/passwd
+echo "--------------------------------"
+echo ""
 read -p "Isikan Username : " Login
 userdel $Login
+echo ""
 echo "Akun $Login berhasil dihapus"
 JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
 echo "--------------------------------"
